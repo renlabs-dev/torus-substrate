f0e5bf32aef42277b705b880254159b9cc74b9ca
Honza <abanan278@gmail.com>
2024-12-10 14:32:45 -0300

    remove: frame-metadata

--- flake.nix
+++ flake.nix
@@ -44,7 +44,8 @@
             OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
             OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
             RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
-          } // nixpkgs.lib.optionalAttrs pkgs.stdenv.isLinux { JEMALLOC_OVERRIDE = "${pkgs.jemalloc}/lib/libjemalloc.so"; };
+          } // nixpkgs.lib.optionalAttrs pkgs.stdenv.isLinux { JEMALLOC_OVERRIDE = "${pkgs.jemalloc}/lib/libjemalloc.so"; }
+          // nixpkgs.lib.optionalAttrs pkgs.stdenv.isDarwin { SKIP_WASM_BUILD = "1"; };
         };
       }
     );
