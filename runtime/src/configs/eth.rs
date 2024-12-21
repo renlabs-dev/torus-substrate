e00138aa683a2a387a02fc40548e36ea0054d21e
Honza <zimulajan0@gmail.com>
2024-12-21 01:05:59 +0100

    wip trying

--- runtime/src/configs/eth.rs
+++ runtime/src/configs/eth.rs
@@ -1,16 +1,15 @@
 use codec::{Decode, Encode};
 use fp_evm::weight_per_gas;
 use pallet_ethereum::PostLogContent;
-use pallet_evm::{EnsureAddressOrigin, HashedAddressMapping};
+use pallet_evm::HashedAddressMapping;
 use polkadot_sdk::{
     frame_support::{parameter_types, traits::FindAuthor},
-    frame_system::RawOrigin,
     pallet_aura,
     polkadot_sdk_frame::prelude::{TransactionValidity, TransactionValidityError},
     sp_core::{ConstU32, H160, U256},
     sp_runtime::{
         traits::{BlakeTwo256, Block as BlockT, DispatchInfoOf, Dispatchable, PostDispatchInfoOf},
-        AccountId32, ConsensusEngineId, DispatchResultWithInfo, Permill, RuntimeAppPublic,
+        ConsensusEngineId, DispatchResultWithInfo, Permill, RuntimeAppPublic,
     },
     sp_weights::Weight,
 };
@@ -37,36 +36,6 @@ impl<F: FindAuthor<u32>> FindAuthor<H160> for FindAuthorTruncated<F> {
     }
 }
 
-pub struct EnsureCuratorAddressTruncated;
-
-impl EnsureAddressOrigin<RuntimeOrigin> for EnsureCuratorAddressTruncated {
-    type Success = AccountId32;
-
-    fn try_address_origin(
-        address: &H160,
-        origin: RuntimeOrigin,
-    ) -> Result<AccountId32, RuntimeOrigin> {
-        <RuntimeOrigin as Into<Result<RawOrigin<AccountId32>, RuntimeOrigin>>>::into(origin)
-            .and_then(|o| match o {
-                RawOrigin::Signed(who) => {
-                    let address_matches = AsRef::<[u8; 32]>::as_ref(&who)[0..20] == address[0..20];
-
-                    if !address_matches {
-                        return Err(RuntimeOrigin::from(RawOrigin::Signed(who)));
-                    }
-
-                    // TODO:
-                    if pallet_governance::Curators::<Runtime>::contains_key(&who) {
-                        return Err(RuntimeOrigin::from(RawOrigin::Signed(who)));
-                    }
-
-                    Ok(who)
-                }
-                r => Err(RuntimeOrigin::from(r)),
-            })
-    }
-}
-
 const WEIGHT_MILLISECS_PER_BLOCK: u64 = 2000;
 
 const BLOCK_GAS_LIMIT: u64 = 75_000_000;
@@ -80,18 +49,17 @@ parameter_types! {
     pub const GasLimitStorageGrowthRatio: u64 = BLOCK_GAS_LIMIT.saturating_div(MAX_STORAGE_GROWTH);
     pub PrecompilesValue: FrontierPrecompiles = FrontierPrecompiles;
     pub WeightPerGas: Weight = Weight::from_parts(weight_per_gas(BLOCK_GAS_LIMIT, NORMAL_DISPATCH_RATIO, WEIGHT_MILLISECS_PER_BLOCK), 0);
-    pub SuicideQuickClearLimit: u32 = 0;
     pub EthereumChainId: u64 = 21000;
 }
 
 impl pallet_evm::Config for Runtime {
-    type AccountProvider = pallet_evm::FrameSystemAccountProvider<Self>;
     type FeeCalculator = BaseFee;
+    type AccountProvider = pallet_evm::FrameSystemAccountProvider<Self>;
     type GasWeightMapping = pallet_evm::FixedGasWeightMapping<Self>;
     type WeightPerGas = WeightPerGas;
     type BlockHashMapping = pallet_ethereum::EthereumBlockHashMapping<Self>;
-    type CallOrigin = EnsureCuratorAddressTruncated;
-    type WithdrawOrigin = EnsureCuratorAddressTruncated;
+    type CallOrigin = pallet_evm::EnsureAddressTruncated;
+    type WithdrawOrigin = pallet_evm::EnsureAddressTruncated;
     type AddressMapping = HashedAddressMapping<BlakeTwo256>;
     type Currency = Balances;
     type RuntimeEvent = RuntimeEvent;
@@ -104,7 +72,7 @@ impl pallet_evm::Config for Runtime {
     type OnCreate = ();
     type FindAuthor = FindAuthorTruncated<Aura>;
     type GasLimitPovSizeRatio = GasLimitPovSizeRatio;
-    type SuicideQuickClearLimit = SuicideQuickClearLimit;
+    type SuicideQuickClearLimit = ConstU32<0>;
     type GasLimitStorageGrowthRatio = GasLimitStorageGrowthRatio;
     type Timestamp = Timestamp;
     type WeightInfo = pallet_evm::weights::SubstrateWeight<Self>;
