9d6fd9441b3295fd5d7be950c4e26393fb49d9be
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2024-12-25 11:19:19 -0300

    test(pallets/torus0): improve stake tests

--- pallets/torus0/tests/stake.rs
+++ pallets/torus0/tests/stake.rs
@@ -10,7 +10,9 @@ fn test_add_stake_correctly() {
     test_utils::new_test_ext().execute_with(|| {
         let from = 0;
         let to = 1;
-        let minimum_stake = <<Test as Config>::DefaultMinimumAllowedStake as Get<u128>>::get();
+        let stake_to_add = <<Test as Config>::DefaultMinimumAllowedStake as Get<u128>>::get();
+        let total_balance = stake_to_add * 2;
+        let balance_after = stake_to_add;
 
         assert_ok!(pallet_torus0::agent::register::<Test>(
             to,
@@ -19,21 +21,21 @@ fn test_add_stake_correctly() {
             "idk".as_bytes().to_vec()
         ));
 
-        let _ = Balances::deposit_creating(&from, minimum_stake * 2);
+        let _ = Balances::deposit_creating(&from, total_balance);
 
-        assert_eq!(Balances::total_balance(&from), minimum_stake * 2);
+        assert_eq!(Balances::total_balance(&from), total_balance);
         assert_eq!(Balances::total_balance(&to), 0);
 
         assert_ok!(pallet_torus0::stake::add_stake::<Test>(
             from,
             to,
-            minimum_stake
+            stake_to_add
         ));
 
-        assert_eq!(Balances::total_balance(&from), minimum_stake);
-        assert_eq!(StakingTo::<Test>::get(&from, &to), Some(minimum_stake));
-        assert_eq!(StakedBy::<Test>::get(&to, &from), Some(minimum_stake));
-        assert_eq!(TotalStake::<Test>::get(), minimum_stake);
+        assert_eq!(Balances::total_balance(&from), balance_after);
+        assert_eq!(StakingTo::<Test>::get(&from, &to), Some(stake_to_add));
+        assert_eq!(StakedBy::<Test>::get(&to, &from), Some(stake_to_add));
+        assert_eq!(TotalStake::<Test>::get(), stake_to_add);
     });
 }
 
@@ -42,19 +44,20 @@ fn test_add_stake_without_registering_the_agent() {
     test_utils::new_test_ext().execute_with(|| {
         let from = 0;
         let to = 1;
-        let minimum_stake = <<Test as Config>::DefaultMinimumAllowedStake as Get<u128>>::get();
+        let stake_to_add = <<Test as Config>::DefaultMinimumAllowedStake as Get<u128>>::get();
+        let total_balance = stake_to_add * 2;
 
-        let _ = Balances::deposit_creating(&from, minimum_stake * 2);
+        let _ = Balances::deposit_creating(&from, total_balance);
 
-        assert_eq!(Balances::total_balance(&from), minimum_stake * 2);
+        assert_eq!(Balances::total_balance(&from), total_balance);
         assert_eq!(Balances::total_balance(&to), 0);
 
         assert_err!(
-            pallet_torus0::stake::add_stake::<Test>(from, to, minimum_stake),
+            pallet_torus0::stake::add_stake::<Test>(from, to, stake_to_add),
             Error::<Test>::AgentDoesNotExist
         );
 
-        assert_eq!(Balances::total_balance(&from), minimum_stake * 2);
+        assert_eq!(Balances::total_balance(&from), total_balance);
         assert_eq!(StakingTo::<Test>::get(&from, &to), None);
         assert_eq!(StakedBy::<Test>::get(&to, &from), None);
         assert_eq!(TotalStake::<Test>::get(), 0);
@@ -66,19 +69,20 @@ fn test_add_stake_without_the_minimum_stake() {
     test_utils::new_test_ext().execute_with(|| {
         let from = 0;
         let to = 1;
-        let minimum_stake = <<Test as Config>::DefaultMinimumAllowedStake as Get<u128>>::get();
+        let stake_to_add = <<Test as Config>::DefaultMinimumAllowedStake as Get<u128>>::get();
+        let total_balance = stake_to_add * 2;
 
-        let _ = Balances::deposit_creating(&from, minimum_stake * 2);
+        let _ = Balances::deposit_creating(&from, total_balance);
 
-        assert_eq!(Balances::total_balance(&from), minimum_stake * 2);
+        assert_eq!(Balances::total_balance(&from), total_balance);
         assert_eq!(Balances::total_balance(&to), 0);
 
         assert_err!(
-            pallet_torus0::stake::add_stake::<Test>(from, to, minimum_stake - 1),
+            pallet_torus0::stake::add_stake::<Test>(from, to, stake_to_add - 1),
             Error::<Test>::StakeTooSmall
         );
 
-        assert_eq!(Balances::total_balance(&from), minimum_stake * 2);
+        assert_eq!(Balances::total_balance(&from), total_balance);
         assert_eq!(StakingTo::<Test>::get(&from, &to), None);
         assert_eq!(StakedBy::<Test>::get(&to, &from), None);
         assert_eq!(TotalStake::<Test>::get(), 0);
@@ -90,7 +94,9 @@ fn test_remove_stake_correctly() {
     test_utils::new_test_ext().execute_with(|| {
         let from = 0;
         let to = 1;
-        let minimum_stake = <<Test as Config>::DefaultMinimumAllowedStake as Get<u128>>::get();
+        let stake_to_add_and_remove =
+            <<Test as Config>::DefaultMinimumAllowedStake as Get<u128>>::get();
+        let total_balance = stake_to_add_and_remove * 2;
 
         assert_ok!(pallet_torus0::agent::register::<Test>(
             to,
@@ -99,23 +105,22 @@ fn test_remove_stake_correctly() {
             "idk".as_bytes().to_vec()
         ));
 
-        let _ = Balances::deposit_creating(&from, minimum_stake * 2);
-
+        let _ = Balances::deposit_creating(&from, total_balance);
         assert_eq!(Balances::total_balance(&to), 0);
 
         assert_ok!(pallet_torus0::stake::add_stake::<Test>(
             from,
             to,
-            minimum_stake
+            stake_to_add_and_remove
         ));
 
         assert_ok!(pallet_torus0::stake::remove_stake::<Test>(
             from,
             to,
-            minimum_stake
+            stake_to_add_and_remove
         ));
 
-        assert_eq!(Balances::total_balance(&from), minimum_stake * 2);
+        assert_eq!(Balances::total_balance(&from), total_balance);
         assert_eq!(StakingTo::<Test>::get(&from, &to), Some(0));
         assert_eq!(StakedBy::<Test>::get(&to, &from), Some(0));
         assert_eq!(TotalStake::<Test>::get(), 0);
@@ -127,7 +132,9 @@ fn test_remove_stake_with_less_than_required_amount() {
     test_utils::new_test_ext().execute_with(|| {
         let from = 0;
         let to = 1;
-        let minimum_stake = <<Test as Config>::DefaultMinimumAllowedStake as Get<u128>>::get();
+        let stake_to_add_and_remove =
+            <<Test as Config>::DefaultMinimumAllowedStake as Get<u128>>::get();
+        let total_balance = stake_to_add_and_remove * 2;
 
         assert_ok!(pallet_torus0::agent::register::<Test>(
             to,
@@ -136,25 +143,30 @@ fn test_remove_stake_with_less_than_required_amount() {
             "idk".as_bytes().to_vec()
         ));
 
-        let _ = Balances::deposit_creating(&from, minimum_stake * 2);
-
+        let _ = Balances::deposit_creating(&from, total_balance);
         assert_eq!(Balances::total_balance(&to), 0);
 
         assert_ok!(pallet_torus0::stake::add_stake::<Test>(
             from,
             to,
-            minimum_stake
+            stake_to_add_and_remove
         ));
 
         assert_err!(
-            pallet_torus0::stake::remove_stake::<Test>(from, to, minimum_stake - 1),
+            pallet_torus0::stake::remove_stake::<Test>(from, to, stake_to_add_and_remove - 1),
             Error::<Test>::StakeTooSmall,
         );
 
-        assert_eq!(Balances::total_balance(&from), minimum_stake);
-        assert_eq!(StakingTo::<Test>::get(&from, &to), Some(minimum_stake));
-        assert_eq!(StakedBy::<Test>::get(&to, &from), Some(minimum_stake));
-        assert_eq!(TotalStake::<Test>::get(), minimum_stake);
+        assert_eq!(Balances::total_balance(&from), stake_to_add_and_remove);
+        assert_eq!(
+            StakingTo::<Test>::get(&from, &to),
+            Some(stake_to_add_and_remove)
+        );
+        assert_eq!(
+            StakedBy::<Test>::get(&to, &from),
+            Some(stake_to_add_and_remove)
+        );
+        assert_eq!(TotalStake::<Test>::get(), stake_to_add_and_remove);
     });
 }
 
@@ -163,7 +175,9 @@ fn test_remove_stake_with_unregistered_agent() {
     test_utils::new_test_ext().execute_with(|| {
         let from = 0;
         let to = 1;
-        let minimum_stake = <<Test as Config>::DefaultMinimumAllowedStake as Get<u128>>::get();
+        let stake_to_add_and_remove =
+            <<Test as Config>::DefaultMinimumAllowedStake as Get<u128>>::get();
+        let total_balance = stake_to_add_and_remove * 2;
 
         assert_ok!(pallet_torus0::agent::register::<Test>(
             to,
@@ -172,25 +186,24 @@ fn test_remove_stake_with_unregistered_agent() {
             "idk".as_bytes().to_vec()
         ));
 
-        let _ = Balances::deposit_creating(&from, minimum_stake * 2);
+        let _ = Balances::deposit_creating(&from, total_balance);
 
         assert_eq!(Balances::total_balance(&to), 0);
-
         assert_ok!(pallet_torus0::stake::add_stake::<Test>(
             from,
             to,
-            minimum_stake
+            stake_to_add_and_remove,
         ));
         assert_ok!(pallet_torus0::agent::unregister::<Test>(to));
 
         assert_err!(
-            pallet_torus0::stake::remove_stake::<Test>(from, to, minimum_stake),
+            pallet_torus0::stake::remove_stake::<Test>(from, to, stake_to_add_and_remove),
             Error::<Test>::AgentDoesNotExist,
         );
 
-        assert_eq!(Balances::total_balance(&from), minimum_stake);
+        assert_eq!(Balances::total_balance(&from), stake_to_add_and_remove);
         assert_eq!(StakingTo::<Test>::get(&from, &to), None);
         assert_eq!(StakedBy::<Test>::get(&to, &from), None);
-        assert_eq!(TotalStake::<Test>::get(), minimum_stake);
+        assert_eq!(TotalStake::<Test>::get(), stake_to_add_and_remove);
     });
 }
