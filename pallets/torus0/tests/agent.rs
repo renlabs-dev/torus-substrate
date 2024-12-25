ff27292e60025378dc128a258f56aedd8ef8b8ab
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2024-12-25 11:47:13 -0300

    test(pallets/torus0): add unregister test

--- pallets/torus0/tests/agent.rs
+++ pallets/torus0/tests/agent.rs
@@ -1,4 +1,3 @@
-use polkadot_sdk::sp_runtime::BoundedVec;
 use test_utils::{assert_ok, Test};
 
 #[test]
@@ -21,5 +20,26 @@ fn test_register_correctly() {
         assert_eq!(agent.name.to_vec(), name);
         assert_eq!(agent.url.to_vec(), url);
         assert_eq!(agent.metadata.to_vec(), metadata);
+    });
+}
+
+#[test]
+fn test_unregister_correctly() {
+    test_utils::new_test_ext().execute_with(|| {
+        let agent = 0;
+        let name = "agent".as_bytes().to_vec();
+        let url = "idk://agent".as_bytes().to_vec();
+        let metadata = "idk://agent".as_bytes().to_vec();
+
+        assert_ok!(pallet_torus0::agent::register::<Test>(
+            agent,
+            name.clone(),
+            url.clone(),
+            metadata.clone(),
+        ));
+
+        assert_ok!(pallet_torus0::agent::unregister::<Test>(agent));
+
+        assert!(pallet_torus0::Agents::<Test>::get(agent).is_none());
     });
 }
