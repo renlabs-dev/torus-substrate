73ae8692e4781dc88ae98bc62320972e2c1036a0
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2024-12-25 11:40:59 -0300

    test(pallets/torus0): add base agent register test

--- pallets/torus0/tests/agent.rs
+++ pallets/torus0/tests/agent.rs
@@ -1,1 +1,25 @@
+use polkadot_sdk::sp_runtime::BoundedVec;
+use test_utils::{assert_ok, Test};
 
+#[test]
+fn test_register_correctly() {
+    test_utils::new_test_ext().execute_with(|| {
+        let agent = 0;
+        let name = "agent".as_bytes().to_vec();
+        let url = "idk://agent".as_bytes().to_vec();
+        let metadata = "idk://agent".as_bytes().to_vec();
+
+        assert_ok!(pallet_torus0::agent::register::<Test>(
+            agent,
+            name.clone(),
+            url.clone(),
+            metadata.clone(),
+        ));
+
+        let agent = pallet_torus0::Agents::<Test>::get(agent).expect("it should exists");
+
+        assert_eq!(agent.name.to_vec(), name);
+        assert_eq!(agent.url.to_vec(), url);
+        assert_eq!(agent.metadata.to_vec(), metadata);
+    });
+}
