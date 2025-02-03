818d940ca7320eb72458ae6e024918b649c824c8
functor-flow <115138297+functor-flow@users.noreply.github.com>
2025-02-03 17:59:43 +0100

    Chore: SPEC VERSION 10

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -39,7 +39,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("torus-runtime"),
     impl_name: create_runtime_str!("torus-runtime"),
     authoring_version: 1,
-    spec_version: 9,
+    spec_version: 10,
     impl_version: 1,
     apis: apis::RUNTIME_API_VERSIONS,
     transaction_version: 1,
