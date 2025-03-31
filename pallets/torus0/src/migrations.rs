fc32e30284a551f73b930a4d5164920554d62989
devwckd <dev.wckd@gmail.com>
2025-03-31 00:49:16 -0300

    feat: add migration

--- pallets/torus0/src/migrations.rs
+++ pallets/torus0/src/migrations.rs
@@ -19,3 +19,62 @@ pub mod v2 {
         }
     }
 }
+
+pub mod v3 {
+    use super::*;
+    use crate::{Agent, Agents};
+    use scale_info::prelude::vec::Vec;
+
+    pub mod storage {
+        use codec::{Decode, Encode, MaxEncodedLen};
+        use polkadot_sdk::frame_support::{storage_alias, DebugNoBound, Identity};
+        use polkadot_sdk::polkadot_sdk_frame::prelude::BlockNumberFor;
+        use polkadot_sdk::sp_runtime::{BoundedVec, Percent};
+        use scale_info::TypeInfo;
+
+        use crate::AccountIdOf;
+
+        #[derive(DebugNoBound, Encode, Decode, MaxEncodedLen, TypeInfo)]
+        #[scale_info(skip_type_params(T))]
+        pub struct Agent<T: crate::Config> {
+            pub key: AccountIdOf<T>,
+            pub name: BoundedVec<u8, T::MaxAgentNameLengthConstraint>,
+            pub url: BoundedVec<u8, T::MaxAgentUrlLengthConstraint>,
+            pub metadata: BoundedVec<u8, T::MaxAgentMetadataLengthConstraint>,
+            pub weight_penalty_factor: Percent,
+            pub registration_block: BlockNumberFor<T>,
+            pub fees: crate::fee::ValidatorFee<T>,
+        }
+
+        #[storage_alias]
+        pub type Agents<T: crate::Config> =
+            StorageMap<crate::Pallet<T>, Identity, AccountIdOf<T>, Agent<T>>;
+    }
+
+    pub type Migration<T, W> = VersionedMigration<2, 3, MigrateToV3<T>, Pallet<T>, W>;
+    pub struct MigrateToV3<T>(core::marker::PhantomData<T>);
+    impl<T: Config> UncheckedOnRuntimeUpgrade for MigrateToV3<T> {
+        fn on_runtime_upgrade() -> Weight {
+            let old_agents = storage::Agents::<T>::iter().collect::<Vec<_>>();
+            let _ = storage::Agents::<T>::clear(u32::MAX, None);
+
+            for (id, old_agent) in old_agents {
+                Agents::<T>::insert(
+                    id,
+                    Agent {
+                        key: old_agent.key,
+                        name: old_agent.name,
+                        url: old_agent.url,
+                        metadata: old_agent.metadata,
+                        registration_block: old_agent.registration_block,
+                        weight_penalty_factor: old_agent.weight_penalty_factor,
+                        fees: old_agent.fees,
+                        last_update_block: None,
+                    },
+                )
+            }
+
+            Weight::zero()
+        }
+    }
+}
